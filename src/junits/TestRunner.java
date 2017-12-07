ed71b3f369923c4e437bd67fc9e9afaad453b931
User <J:\Soft\TheBat!\TheBat!>
2017-12-07 08:53:42 +0300

    first commit

---
+++ src/junits/TestRunner.java
@@ -0,0 +1,23 @@
+package junits;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+ 
+public class TestRunner {
+ 
+	public static void main(String[] args) {
+
+		Result result = JUnitCore.runClasses(JunitMathProvider_1.class, JunitMathProvider_2.class);
+		System.out.println("Total number of tests " + result.getRunCount());
+		System.out.println("Total number of tests failed " + result.getFailureCount());
+ 
+		for(Failure failure : result.getFailures())
+		{	
+			System.out.println(failure.getMessage());
+		}
+		System.out.println(result.wasSuccessful());
+ 
+	}
+ 
+}
